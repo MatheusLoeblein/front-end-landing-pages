fec805d868bf20538df45e3b3bbbb4135e4b5ad1
Matheus <matheus.eai@gmail.com>
2023-06-22 22:16:54 -0300

    Fix border menu

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -15,7 +15,7 @@ export const Container = styled.div`
     left: 0;
     right: 0;
     width: 100%;
-    border-bottom: ${theme.colors.mediumGray};
+    border-bottom: 0.1rem solid ${theme.colors.mediumGray};
     background: ${theme.colors.white};
     transition: all 300ms ease-in-out;
 
